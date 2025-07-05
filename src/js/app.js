bc38485d7c11a25c19818c2e45f58c798aa63b31
BuildTools <unconfigured@null.spigotmc.org>
2025-07-05 07:03:03 +0600

    url validation

---
+++ src/js/app.js
@@ -0,0 +1,64 @@
+import '../scss/styles.scss';
+import 'bootstrap';
+import * as yup from 'yup';
+import onChange from 'on-change';
+import render from './render.js';
+
+const app = () => {
+  const state = {
+    formState: 'filling',
+    inputValue: '',
+  };
+
+  const elements = {
+    form: document.querySelector('form'),
+    input: document.querySelector('#url-input'),
+    submit: document.querySelector('[type="submit"]'),
+    feedback: document.querySelector('.feedback'),
+    feeds: document.querySelector('.feeds'),
+    posts: document.querySelector('.posts'),
+    modalHeader: document.querySelector('.modal-header'),
+    modalBody: document.querySelector('.modal-body'),
+    modalButtons: document.querySelectorAll('.btn-outline-primary'),
+  };
+
+  const watchedState = onChange(state, path => render(path, state, elements));
+
+  const validateURL = (url, existingLinks) => {
+    const schema = yup.string().required().url().notOneOf(existingLinks);
+
+    return schema
+      .validate(url)
+      .then(() => null)
+      .catch((error) => {
+        return error;
+      });
+  };
+
+  elements.form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    watchedState.formState = 'filling';
+    const formData = new FormData(e.target);
+    const url = formData.get('url');
+    watchedState.inputValue = url;
+
+    validateURL(url, [])
+      .then((error) => {
+        if (error) {
+          watchedState.error = error;
+          watchedState.formState = 'invalid';
+          throw new Error(error);
+        }
+        else {
+          watchedState.error = null;
+          return url;
+        }
+      })
+      .then((link) => {
+        watchedState.formState = 'sending';
+      });
+  });
+};
+
+app();
---
+++ src/js/render.js
@@ -0,0 +1,47 @@
+const renderInput = (state, elements) => {
+  switch (state.formState) {
+    case 'filling':
+      elements.submit.disabled = false;
+      elements.input.classList.remove('is-invalid');
+      elements.feedback.textContent = '';
+      elements.feedback.classList.remove('text-danger');
+      elements.feedback.classList.remove('text-success');
+      break
+    case 'valid':
+      elements.submit.disabled = false;
+      elements.input.value = '';
+      elements.input.classList.remove('is-invalid');
+      elements.feedback.classList.remove('text-danger');
+      elements.feedback.classList.add('text-success');
+      elements.feedback.textContent = 'status.success';
+      break
+    case 'invalid':
+      elements.submit.disabled = false;
+      elements.input.value = state.inputValue;
+      elements.input.classList.add('is-invalid');
+      elements.feedback.textContent = 'state.error';
+      elements.feedback.classList.remove('text-success');
+      elements.feedback.classList.add('text-danger');
+      break
+    case 'sending':
+      elements.submit.disabled = true;
+      elements.input.value = state.inputValue;
+      elements.input.classList.remove('is-invalid');
+      elements.feedback.textContent = '';
+      elements.feedback.classList.remove('text-danger');
+      elements.feedback.classList.remove('text-success');
+      break
+    default:
+      break
+  }
+}
+
+export default (path, state, elements) => {
+  switch (path) {
+    case 'formState':
+      renderInput(state, elements);
+      break;
+    default:
+      break;
+  }
+}
